ed6c1d3ad56805fdab29fc4af70bd2d4a85d23bc
Airyyy23 <wayangseno123@gmail.com>
2023-08-07 09:40:32 +0700

    revisi[WL] Homepage

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter_2/DayPage.dart';
 import 'package:latihan_flutter_2/MonthPage.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,28 +13,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Home Page',
-          style: TextStyle(
-            fontWeight: FontWeight.bold
+        appBar: AppBar(
+          title: Text(
+            'Home Page',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            child: ElevatedButton(
-              onPressed: _goToMonthPage,
-              child: Text("data"),
-            ),
-          )
-        ],
-      ),
-    );
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: _goToMonthPage,
+                  child: Text("Data Bulan"),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: _goToDayPage,
+                  child: Text("Data Hari"),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
   void _goToMonthPage() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Monthpage()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Monthpage()));
+  }
+
+  void _goToDayPage() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => DayPage()));
   }
 }
